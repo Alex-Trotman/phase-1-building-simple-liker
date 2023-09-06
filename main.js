6a5785c173b04a44cb85d6c4af8d0705ff87ad92
Alex Trotman <alextrotman04@gmail.com>
2023-09-06 08:56:25 -0500

    removes comments

--- main.js
+++ main.js
@@ -4,43 +4,18 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
-
-// function likeCallback(e) {
-//   const heart = e.target;
-//   mimicServerCall()
-//     .then(function(serverMessage){
-//       alert(serverMessage);
-//       heart.innerText = glyphStates[heart.innerText];
-//       heart.style.color = colorStates[heart.style.color];
-//     })
-//     .catch(function(error) {
-//       const hiddenModal = document.getElementsByClassName('hidden');
-//       console.log(hiddenModal)
-//       hiddenModal.classList.remove('hidden')
-//     });
-// }
-
-
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
-
-// Your JavaScript code goes here!
-
 const articleHearts = document.querySelectorAll(".like-glyph");
 const errorModal = document.querySelector("#modal");
 const errorMessage = document.querySelector("#modal-message");
 
-// Define the states (you need to provide these values)
 const glyphStates = {
   '♡': '♥',
   '♥': '♡'
 };
 
 const colorStates = {
-  'red': '', // Define the color changes here
-  '': 'red'  // Define the color changes here
+  'red': '', 
+  '': 'red'  
 };
 
 function likeCallback(e) {
@@ -52,10 +27,10 @@ function likeCallback(e) {
       heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
-      errorMessage.textContent = error; // Display the error message
-      errorModal.classList.remove('hidden'); // Remove the .hidden class
+      errorMessage.textContent = error; 
+      errorModal.classList.remove('hidden'); 
       setTimeout(function () {
-        errorModal.classList.add('hidden'); // Hide the modal after 3 seconds
+        errorModal.classList.add('hidden'); 
       }, 3000);
     });
 }
