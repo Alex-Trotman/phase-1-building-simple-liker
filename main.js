b4352c03b74e29cccadae486af53297fec4f8fb3
Alex Trotman <alextrotman04@gmail.com>
2023-09-06 08:54:20 -0500

    passes

--- main.js
+++ main.js
@@ -3,11 +3,67 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-document.addEventListener("DOMContentLoaded", function() {
-  const likeButton = document.getElementsByClassName("like-glyph");
-  likeButton.addEventListener("click", function() {
-    console.log("Button clicked!");})
-});
+
+// const articleHearts = document.querySelectorAll(".like-glyph");
+
+// function likeCallback(e) {
+//   const heart = e.target;
+//   mimicServerCall()
+//     .then(function(serverMessage){
+//       alert(serverMessage);
+//       heart.innerText = glyphStates[heart.innerText];
+//       heart.style.color = colorStates[heart.style.color];
+//     })
+//     .catch(function(error) {
+//       const hiddenModal = document.getElementsByClassName('hidden');
+//       console.log(hiddenModal)
+//       hiddenModal.classList.remove('hidden')
+//     });
+// }
+
+
+// for (const glyph of articleHearts) {
+//   glyph.addEventListener("click", likeCallback);
+// }
+
+// Your JavaScript code goes here!
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+const errorModal = document.querySelector("#modal");
+const errorMessage = document.querySelector("#modal-message");
+
+// Define the states (you need to provide these values)
+const glyphStates = {
+  '♡': '♥',
+  '♥': '♡'
+};
+
+const colorStates = {
+  'red': '', // Define the color changes here
+  '': 'red'  // Define the color changes here
+};
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      errorMessage.textContent = error; // Display the error message
+      errorModal.classList.remove('hidden'); // Remove the .hidden class
+      setTimeout(function () {
+        errorModal.classList.add('hidden'); // Hide the modal after 3 seconds
+      }, 3000);
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
